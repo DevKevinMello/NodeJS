2ccae730432607e2f0273d4499ecd61f92a70f65
Kevin Mello <kevinflat23@hotmail.com>
2023-07-16 21:49:59 -0300

    autenticacao jwt com passport, mais comum de se utilizar

--- NODE-AUTH-PASSPORT/src/config/passport.ts
+++ NODE-AUTH-PASSPORT/src/config/passport.ts
@@ -1,5 +1,57 @@
+import { Request, Response, NextFunction } from 'express';
 import passport from "passport";
+import dotenv from 'dotenv';
+import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
+import jwt from 'jsonwebtoken';
+import { User, UserInstance } from '../models/User';
 
-//configurar
+dotenv.config();
+
+const notAuthorizedJson = { status: 401, message: 'Não autorizado' };
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET as string
+}
+
+passport.use(new JWTStrategy(options, async (payload, done) => {
+    const user = await User.findByPk(payload.id);
+    return user ? done(null, user) : done(notAuthorizedJson, false);
+}));
+
+export const generateToken = (data: object) => {
+    return jwt.sign(data, process.env.JWT_SECRET as string);
+}
+
+export const privateRoute = (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('jwt', (err: Error, user: UserInstance) => {
+        req.user = user;
+        return user ? next() : next(notAuthorizedJson);
+    })(req, res, next);
+}
+
+/* BASIC AUTH
+passport.use(new BasicStrategy( async (email, password, done) => {
+
+    if( email && password ){
+        const user = await User.findOne({
+            where: { email, password }
+        });
+
+        if(user){
+            return done(null, user);
+        }
+    }
+
+    return done(notAuthorizedJson, false);
+}));
+
+export const privateRoute = (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('basic', (err: Error, user: UserInstance) => {
+        req.user = user;
+        return user ? next() : next(notAuthorizedJson);
+    })(req, res, next);
+}
+*/
 
 export default passport;
--- NODE-AUTH-PASSPORT/src/controllers/apiController.ts
+++ NODE-AUTH-PASSPORT/src/controllers/apiController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { User } from '../models/User';
+import { generateToken } from '../config/passport';
 
 export const ping = (req: Request, res: Response) => {
     res.json({pong: true});
@@ -13,8 +14,10 @@ export const register = async (req: Request, res: Response) => {
         if(!hasUser) {
             let newUser = await User.create({ email, password });
 
+            const token = generateToken({ id: newUser.id });
+
             res.status(201);
-            res.json({ id: newUser.id });
+            res.json({ id: newUser.id, token });
         } else {
             res.json({ error: 'E-mail já existe.' });
         }
@@ -33,7 +36,8 @@ export const login = async (req: Request, res: Response) => {
         });
 
         if(user) {
-            res.json({ status: true });
+            const token = generateToken({ id: user.id });
+            res.json({ status: true, token });
             return;
         }
     }
